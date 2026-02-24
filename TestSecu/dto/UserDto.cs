9109e1b7dedd09ac15fe6837e5474086d3f8f8d5
michael person <michael.person@cognitic.be>
2026-02-24 16:58:26 +0100

    Changement de l'expression régulière pour le mot de passe car elle n'autorisait pas le =

--- TestSecu/dto/UserDto.cs
+++ TestSecu/dto/UserDto.cs
@@ -11,8 +11,8 @@ namespace TestSecu.dto
         [Required]
         [MaxLength(100)]
         [MinLength(10)]
-        //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")]
-        
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{10,}$",
+            ErrorMessage = "Password must be at least 10 characters and include at least 1 lowercase letter, 1 uppercase letter, 1 digit, and 1 special character.")]
         public required string Password { get; set; }
     }
 }
