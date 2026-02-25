5f9f39dd62c91403517b50fb1adc2a233c07bfeb
michael person <michael.person@cognitic.be>
2026-02-25 08:52:14 +0100

    Change regex for password

--- TestSecu.Infrastructure/Repositories/AccountRepository.cs
+++ TestSecu.Infrastructure/Repositories/AccountRepository.cs
@@ -14,14 +14,14 @@ namespace TestSecu.Infrastructure.Repositories
             if (email is null || password is null) return false;
             Regex reg = new Regex("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$");
             if (!reg.IsMatch(email)) return false;
-            Regex regPassword = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{2,}$");
+            Regex regPassword = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).{10,}$");
             if (!regPassword.IsMatch(password)) return false;
 
 
             //Fakes users
             List<User> users = new List<User>()
             {
-                new User() { Id=1, Email="zorro@tftic.be", Password="SecureAdmin789!" },
+                new User() { Id=1, Email="zorro@tftic.be", Password="Test1234=!" },
                 new User() { Id=2, Email="tornado@tftic.be", Password="SecureAdmin789!" },
 
             };
